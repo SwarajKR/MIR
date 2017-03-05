997c3a725bfa5660fb385c543be3525fe9b90dd0
SwarajKR <swarajkrishnan@outlook.com>
2017-03-05 20:46:41 +0530

    clustering the songs based on songs

--- src/main.py
+++ src/main.py
@@ -1,82 +1,155 @@
-#NOTE
-
-# MAKE Process Multiprocessing Threading Only Uses  one core at time
-#perfomance will improve considerably
-
-#IMPORTS
 from essentia.standard import MonoLoader
-from essentia.standard import Extractor
+from essentia.standard import LowLevelSpectralEqloudExtractor
+from essentia.standard import LowLevelSpectralExtractor
+from essentia.standard import Loudness
+from essentia.standard import RhythmExtractor2013
+from essentia.standard import KeyExtractor
+from essentia.standard import LogAttackTime
 import essentia.streaming
 import numpy as np
-from os import listdir
-from os.path import isfile, join
+from math import sqrt
 import re
-import threading
-import time
+from os import listdir
+from multiprocessing import Pool
+from random import randint
 
-#AVG FUNCTION TO FIND LIST AVG
+def euclidieanDistance(listOne,listTwo):
+	distance = 0
+	length = len(listOne)
+	for i in range(length):
+		distance += (listOne[i]-listTwo[i])*(listOne[i]-listTwo[i])
+	return sqrt(distance)
+
+def lowLevel(songName):
+	loader = MonoLoader(filename = songName)
+	audio = loader()
+	extractor = LowLevelSpectralEqloudExtractor()
+	feature =list(extractor(audio))
+	del feature[1];del feature[1]
+	extractor =LowLevelSpectralExtractor()
+	featureTwo = list(extractor(audio))
+	del  featureTwo[0]
+	del featureTwo[-2]
+	featureTwo[4] = feature[4][1]
+	feature.extend(featureTwo)
+	extractor = Loudness()
+	feature.append(extractor(audio))
+	extractor = LogAttackTime()
+	feature.append(extractor(audio)[0])
+	extractor = KeyExtractor()
+	feature.append(extractor(audio)[2])
+	extractor = RhythmExtractor2013()
+	data = extractor(audio)
+	feature.append(data[0])
+	feature.append(data[2])
+	for x in  range(len(feature)) :
+		if type(feature[x]) is  np.ndarray :
+			feature[x] = avg(feature[x])
+	return feature
+	
 def avg(arr):
-	return sum(arr)/len(arr)
+    return sum(arr)/len(arr)
+   
+def normalize(val,norm):
+ 	for i in range(len(val)):
+    		val[i] = ((val[i]- norm[i][1])/(norm[i][0]-norm[i][1]))*(100-0.1)+0.1
+    	return val
 
-#THE FUNCTION WHICH PERFOMS MIR AND STRORES INTO 
-class  findFeature(threading.Thread):
-		def __init__(self,songName):
-			 threading.Thread.__init__(self)
-			 self.name = songName
-		def run(self):
-			#LOAD THE SONG
-			loader = MonoLoader(filename = self.name)
-			audio = loader()
-			extractor =  Extractor()
-			#Return a pool (DS WITH ALL THE  MUSIC FEATUES VALUES )
-			pool = extractor(audio)
-			name=self.name.split("/")[-1]
-			arr = []
-			arr.append(name)
-			#LIST OF FEATURE NAMES
-			features = ['lowLevel.average_loudness', 'rhythm.bpm', 'rhythm.confidence', 'rhythm.onset_rate', 'sfx.pitch_after_max_to_before_max_energy_ratio', 'sfx.pitch_centroid', 'sfx.pitch_max_to_total', 'sfx.pitch_min_to_total', 'tonal.chords_changes_rate', 'tonal.chords_number_rate', 'tonal.key_strength', 'tonal.tuning_diatonic_strength', 'tonal.tuning_equal_tempered_deviation', 'tonal.tuning_frequency', 'tonal.tuning_nontempered_energy_ratio', 'lowLevel.barkbands_kurtosis', 'lowLevel.barkbands_skewness', 'lowLevel.barkbands_spread', 'lowLevel.dissonance', 'lowLevel.hfc', 'lowLevel.pitch', 'lowLevel.pitch_instantaneous_confidence', 'lowLevel.pitch_salience', 'lowLevel.silence_rate_20dB', 'lowLevel.silence_rate_30dB', 'lowLevel.silence_rate_60dB', 'lowLevel.spectral_centroid', 'lowLevel.spectral_complexity', 'lowLevel.spectral_crest', 'lowLevel.spectral_decrease', 'lowLevel.spectral_energy', 'lowLevel.spectral_energyband_high', 'lowLevel.spectral_energyband_low', 'lowLevel.spectral_energyband_middle_high', 'lowLevel.spectral_energyband_middle_low', 'lowLevel.spectral_flatness_db', 'lowLevel.spectral_flux', 'lowLevel.spectral_kurtosis', 'lowLevel.spectral_rms', 'lowLevel.spectral_rolloff', 'lowLevel.spectral_skewness', 'lowLevel.spectral_spread', 'lowLevel.spectral_strongpeak', 'lowLevel.zerocrossingrate', 'rhythm.beats_loudness', 'rhythm.first_peak_bpm', 'rhythm.first_peak_spread', 'rhythm.first_peak_weight', 'rhythm.second_peak_bpm', 'rhythm.second_peak_spread', 'rhythm.second_peak_weight', 'sfx.inharmonicity', 'sfx.oddtoevenharmonicenergyratio', 'tonal.chords_strength', 'rhythm.bpm_intervals', 'tonal.chords_histogram', 'tonal.thpcp', 'tonal.chords_key', 'tonal.chords_scale', 'tonal.key_key', 'tonal.key_scale']
-			#ELIMINATED MUSIC FEATURES
-			#'tonal.chords_progression' ,  'rhythm.histogram','rhythm.onset_times','rhythm.beats_position', , 'rhythm.bpm_estimates'
-			#Temperory Eliminations
-			# 'lowLevel.barkbands', 'lowLevel.mfcc', 'lowLevel.sccoeffs', 'lowLevel.scvalleys', 'rhythm.beats_loudness_band_ratio', 'sfx.tristimulus', 'tonal.hpcp',
-			for i in features :
-				if  type(pool[i]) is np.ndarray:
-					arr.append(avg(pool[i]))
-				else :
-		 		   arr.append(pool[i])
-			arr =str(arr)
-			arr =  arr[1:-1]+"\n"
-			f=  open('dataset.csv','a')
-			lock.acquire()
-			f.write(arr)
-			print "Added",self.name
-			lock.release()
-			f.close()
-			
-#Find the  Songs in Specified directory and Return them as a list
+def findCost(medoid,names,distance):
+	cost = 0
+	for i in range(len(names)):
+		temp = distance[medoid[0]][i]
+		for x in medoid:
+			if distance[x][i] < temp:
+				temp = distance[x][i]
+		cost += temp
+	return cost
+
+def recluster(medoid,names,distance,numCluster,clusters):
+	medNum = 0
+	newMedoids = []
+	newMedoids.extend(medoid)
+	newCost = 0
+	rMedoid = []
+	while(medNum < len(medoid)):
+		arr=clusters[medoid[medNum]]
+		for start in range(len(arr)):
+			newMedoids[medNum] = arr[start]
+			cost = findCost(newMedoids,names,distance)
+			if  cost < newCost or newCost == 0: 
+				newCost = cost
+				rMedoid = []
+				rMedoid.extend(newMedoids)
+			start += 1
+		for x in range(len(newMedoids)):
+			newMedoids[x] = medoid[x]
+		medNum += 1
+	return newCost,rMedoid
+
+def cluster(medoid,names,distance,numCluster):
+	clusters = {}
+	for med in medoid:
+		clusters[med] = []
+	cost=0;
+	for i in range(len(names)):
+		temp = distance[medoid[0]][i]
+		closestMedoid = medoid[0]
+		for x in medoid:
+			if distance[x][i] < temp:
+				temp = distance[x][i]
+				closestMedoid = x
+		cost += temp
+		clusters[closestMedoid].append(names[i])
+	newCost,newMedoids = recluster(medoid,names,distance,numCluster,clusters)
+	if newCost < cost:
+		cost,clusters = cluster(newMedoids,names,distance,numCluster)
+	return cost,clusters
+
 def findMusic():
-        print "Enter The path : example /home/swaraj/Music \n"
-	myPath =  raw_input()
+	print "Enter The path : example /home/swaraj/Music \n"
+	#myPath = raw_input()
+	#myPath = "/home/swaraj/workspace/test"
+	myPath = "/home/swaraj/Music"
 	files = [myPath+"/"+file for file in listdir(myPath) if  re.match(r'(.*)mp3',file)]
 	return files
-
-#THE MAIN METHOD FOR UNIT TESTING
+    	
 def main():
-	fileList  =  findMusic()
-	count =  0
-	threads = []
-	for song in fileList :
-		threads.append( findFeature(song))
-		count+=1
-		threads[-1].start()
-		if(count == 6):
-			count = 0
-			for thread  in threads: 
-				thread.join()
-			del threads[0:5]
-	for  t in  range(len(threads)):
-		 t.join()
-		 
-#LOCK FOR SYNC
-lock = threading.Lock()
+	names = []
+	extract = Pool(4)
+	featureList = {}
+	songList = findMusic()
+	extractedList = extract.map(lowLevel,songList)
+	norm = []
+	for i in zip(*extractedList):
+		norm.append([max(i),min(i)])
+	for x in range(len(songList)):
+		names.append(songList[x].split("/")[-1])
+		featureList[names[-1]] =  normalize(extractedList[x],norm)
+	distance = {}
+	#Find euclidiean Distances
+	for song in names:
+		distance[song] = []
+		current = featureList[song]
+		for songitem in  names:
+			if songitem == song :
+				distance[song].append(0)
+			else:
+				value = featureList[songitem]
+				distance[song].append(euclidieanDistance(current,value))
+	#no of clusters = 2
+	numCluster = 5
+	for i,j in enumerate(names):
+		print i,j
+	#CHOOSE MEDOIDS RANDOMLY FOR THE FIRST TIME
+	medoid = []
+	while(len(medoid) < numCluster):
+		temp = randint(0,len(names)-1)
+		if names[temp] not in medoid:
+			medoid.append(names[temp])	
+	#CALL CLUSTER METHOD
+	cost,clusters = cluster(medoid,names,distance,numCluster)
+	#PRINTING THE OUTPUT
+	print "\n\n",cost
+	for key,value in clusters.iteritems():
+		print "\n",key,":",value
 main()
