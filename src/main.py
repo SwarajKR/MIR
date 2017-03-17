afd3630099582e65688d0dd384d6b365a67300d4
SwarajKR <swarajkrishnan@outlook.com>
2017-03-17 13:35:24 +0000

    Fixed  , in Filename Bug

--- src/main.py
+++ src/main.py
@@ -10,8 +10,24 @@ import numpy as np
 from math import sqrt
 import re
 from os import listdir
-from multiprocessing import Pool
+from multiprocessing import Pool,Lock
 from random import randint
+import csv
+
+dataset = {}
+lock = Lock()
+
+import cPickle as pick
+
+def load():
+    print "\n\tLoading Files....."
+    line = csv.reader(open("data.csv","rb"))
+    datas  =  list(line)
+    global dataset
+    for i in datas:
+    	dataset[i[0]] = [float(x) for x in i[1:]]
+    print "\tLoading Complete...."  
+    return dataset
 
 def euclidieanDistance(listOne,listTwo):
 	distance = 0
@@ -21,32 +37,46 @@ def euclidieanDistance(listOne,listTwo):
 	return sqrt(distance)
 
 def lowLevel(songName):
-	loader = MonoLoader(filename = songName)
-	audio = loader()
-	extractor = LowLevelSpectralEqloudExtractor()
-	feature =list(extractor(audio))
-	del feature[1];del feature[1]
-	extractor =LowLevelSpectralExtractor()
-	featureTwo = list(extractor(audio))
-	del  featureTwo[0]
-	del featureTwo[-2]
-	featureTwo[4] = feature[4][1]
-	feature.extend(featureTwo)
-	extractor = Loudness()
-	feature.append(extractor(audio))
-	extractor = LogAttackTime()
-	feature.append(extractor(audio)[0])
-	extractor = KeyExtractor()
-	feature.append(extractor(audio)[2])
-	extractor = RhythmExtractor2013()
-	data = extractor(audio)
-	feature.append(data[0])
-	feature.append(data[2])
-	for x in  range(len(feature)) :
-		if type(feature[x]) is  np.ndarray :
-			feature[x] = avg(feature[x])
-	return feature
-	
+	global dataset
+	global lock
+	print songName
+	key = re.sub(r',',"",songName.split('/')[-1])
+	if dataset.has_key(key):
+		feature = dataset[key]
+		return feature
+	else:
+		loader = MonoLoader(filename = songName)
+		audio = loader()
+		extractor = LowLevelSpectralEqloudExtractor()
+		feature =list(extractor(audio))
+		del feature[1];del feature[1]
+		extractor =LowLevelSpectralExtractor()
+		featureTwo = list(extractor(audio))
+		del  featureTwo[0]
+		del featureTwo[-2]
+		featureTwo[4] = feature[4][1]
+		feature.extend(featureTwo)
+		extractor = Loudness()
+		feature.append(extractor(audio))
+		extractor = LogAttackTime()
+		feature.append(extractor(audio)[0])
+		extractor = KeyExtractor()
+		feature.append(extractor(audio)[2])
+		extractor = RhythmExtractor2013()
+		data = extractor(audio)
+		feature.append(data[0])
+		feature.append(data[2])
+		for x in  range(len(feature)) :
+			if type(feature[x]) is  np.ndarray :
+				feature[x] = avg(feature[x])
+		arr = key+","+str(feature)[1:-1]+"\n"
+ 		f=  open('data.csv','a')
+ 		lock.acquire()
+ 		f.write(arr)
+ 		lock.release()
+ 		f.close()
+		return feature
+
 def avg(arr):
     return sum(arr)/len(arr)
    
@@ -108,22 +138,27 @@ def cluster(medoid,names,distance,numCluster):
 def findMusic():
 	print "Enter The path : example /home/swaraj/Music \n"
 	#myPath = raw_input()
-	#myPath = "/home/swaraj/workspace/test"
-	myPath = "/home/swaraj/Music"
+	#myPath = "/home/swaraj/Music"
+	myPath = "/home/swaraj/workspace/test"
 	files = [myPath+"/"+file for file in listdir(myPath) if  re.match(r'(.*)mp3',file)]
 	return files
     	
 def main():
+	global dataset
+	dataset = load()
 	names = []
-	extract = Pool(4)
+	extract = Pool(2)
 	featureList = {}
 	songList = findMusic()
 	extractedList = extract.map(lowLevel,songList)
 	norm = []
 	for i in zip(*extractedList):
 		norm.append([max(i),min(i)])
+	######TO BE MODIFIED
+	pick.dump(norm,open("norm.p","wb"))
+	######TO BE MODIFIED
 	for x in range(len(songList)):
-		names.append(songList[x].split("/")[-1])
+		names.append(re.sub(r',',"",songList[x].split("/")[-1]))
 		featureList[names[-1]] =  normalize(extractedList[x],norm)
 	distance = {}
 	#Find euclidiean Distances
@@ -136,20 +171,25 @@ def main():
 			else:
 				value = featureList[songitem]
 				distance[song].append(euclidieanDistance(current,value))
-	#no of clusters = 2
-	numCluster = 5
-	for i,j in enumerate(names):
-		print i,j
-	#CHOOSE MEDOIDS RANDOMLY FOR THE FIRST TIME
-	medoid = []
-	while(len(medoid) < numCluster):
-		temp = randint(0,len(names)-1)
-		if names[temp] not in medoid:
-			medoid.append(names[temp])	
-	#CALL CLUSTER METHOD
-	cost,clusters = cluster(medoid,names,distance,numCluster)
-	#PRINTING THE OUTPUT
-	print "\n\n",cost
-	for key,value in clusters.iteritems():
-		print "\n",key,":",value
+	#CLUSTERING
+	bestCluster = {}
+	bestCost = 0
+	for size in range(2,10):
+		numCluster = size
+		#CHOOSE MEDOIDS RANDOMLY FOR THE FIRST TIME
+		medoid = []
+		while(len(medoid) < numCluster):
+			temp = randint(0,len(names)-1)
+			if names[temp] not in medoid:
+				medoid.append(names[temp])	
+		#CALL CLUSTER METHOD
+		cost,clusters = cluster(medoid,names,distance,numCluster)
+		#PRINTING THE OUTPUT
+		if cost < bestCost or bestCost == 0:
+			bestCost = cost
+			bestCluster =clusters
+	print "\n\n",bestCost
+	for key,value in bestCluster.iteritems():
+			print "\n",key,":",value
+
 main()
