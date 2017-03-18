81c3979af54818ff11abdf22cc07c22f9de8cc87
SwarajKR <swarajkrishnan@outlook.com>
2017-03-18 07:41:04 +0000

    Startted Prediction

--- src/main.py
+++ src/main.py
@@ -19,15 +19,18 @@ lock = Lock()
 
 import cPickle as pick
 
-def load():
-    print "\n\tLoading Files....."
-    line = csv.reader(open("data.csv","rb"))
+
+#Load The data  and return a csv
+
+def load(csvname):
+    line = csv.reader(open(csvname,"rb"))
     datas  =  list(line)
     global dataset
     for i in datas:
     	dataset[i[0]] = [float(x) for x in i[1:]]
-    print "\tLoading Complete...."  
     return dataset
+
+#calculate euclidiean distance between two Songs
 
 def euclidieanDistance(listOne,listTwo):
 	distance = 0
@@ -36,15 +39,20 @@ def euclidieanDistance(listOne,listTwo):
 		distance += (listOne[i]-listTwo[i])*(listOne[i]-listTwo[i])
 	return sqrt(distance)
 
+
+#Extractong Feature Names From Songs
+
 def lowLevel(songName):
 	global dataset
 	global lock
 	print songName
 	key = re.sub(r',',"",songName.split('/')[-1])
+	#IF already present in dataset dont extract
 	if dataset.has_key(key):
 		feature = dataset[key]
 		return feature
 	else:
+		#Loading song and using Extractors
 		loader = MonoLoader(filename = songName)
 		audio = loader()
 		extractor = LowLevelSpectralEqloudExtractor()
@@ -77,13 +85,22 @@ def lowLevel(songName):
  		f.close()
 		return feature
 
+
+#Calculate Average
+
 def avg(arr):
     return sum(arr)/len(arr)
    
+
+#Normalize values Into a Range
+
 def normalize(val,norm):
  	for i in range(len(val)):
     		val[i] = ((val[i]- norm[i][1])/(norm[i][0]-norm[i][1]))*(100-0.1)+0.1
     	return val
+
+
+#Find The Cost (K medoid)
 
 def findCost(medoid,names,distance):
 	cost = 0
@@ -94,6 +111,10 @@ def findCost(medoid,names,distance):
 				temp = distance[x][i]
 		cost += temp
 	return cost
+
+#Find the new medoid and calculate Cost
+#Instead of Choosing random medoid traversed Through all medoids to find one with min error cost
+
 
 def recluster(medoid,names,distance,numCluster,clusters):
 	medNum = 0
@@ -116,6 +137,9 @@ def recluster(medoid,names,distance,numCluster,clusters):
 		medNum += 1
 	return newCost,rMedoid
 
+#Calculate The CLuster and call recluster to find new medoid
+#if new medoid is better than old medoid make that the new cluster
+
 def cluster(medoid,names,distance,numCluster):
 	clusters = {}
 	for med in medoid:
@@ -135,33 +159,47 @@ def cluster(medoid,names,distance,numCluster):
 		cost,clusters = cluster(newMedoids,names,distance,numCluster)
 	return cost,clusters
 
+#FInding song  names From Directory
+
 def findMusic():
 	print "Enter The path : example /home/swaraj/Music \n"
 	#myPath = raw_input()
-	#myPath = "/home/swaraj/Music"
-	myPath = "/home/swaraj/workspace/test"
+	myPath = "/home/swaraj/Music"
+	#myPath = "/home/swaraj/workspace/test"
 	files = [myPath+"/"+file for file in listdir(myPath) if  re.match(r'(.*)mp3',file)]
 	return files
     	
+#The Main Function
+
 def main():
+	#dataset is global since it is used and modified by multiple funcutions
 	global dataset
-	dataset = load()
+	dataset = load("data.csv")
 	names = []
+	#Using pool for using multiple cores of cpu
 	extract = Pool(2)
 	featureList = {}
 	songList = findMusic()
 	extractedList = extract.map(lowLevel,songList)
-	norm = []
+	#load min and max values of The database so that everythig can be normalized
+	fp =open("norm.p","rb")
+	norm = pick.load(fp)
+	fp.close()
+	#Modifying The min and max values to include user values
+	randomTemp = 0
 	for i in zip(*extractedList):
-		norm.append([max(i),min(i)])
-	######TO BE MODIFIED
-	pick.dump(norm,open("norm.p","wb"))
-	######TO BE MODIFIED
+		maxI = max(i)
+		minI = min(i)
+		if norm[randomTemp][0] < maxI:
+			norm[randomTemp][0] = maxI
+		if norm[randomTemp][1] > minI:
+			norm[randomTemp][1] = minI
+		randomTemp += 1
 	for x in range(len(songList)):
 		names.append(re.sub(r',',"",songList[x].split("/")[-1]))
 		featureList[names[-1]] =  normalize(extractedList[x],norm)
 	distance = {}
-	#Find euclidiean Distances
+	#Find euclidiean Distances between all the songs to be used in clustering
 	for song in names:
 		distance[song] = []
 		current = featureList[song]
@@ -184,12 +222,21 @@ def main():
 				medoid.append(names[temp])	
 		#CALL CLUSTER METHOD
 		cost,clusters = cluster(medoid,names,distance,numCluster)
-		#PRINTING THE OUTPUT
 		if cost < bestCost or bestCost == 0:
 			bestCost = cost
 			bestCluster =clusters
+	#OUTPUT
 	print "\n\n",bestCost
 	for key,value in bestCluster.iteritems():
 			print "\n",key,":",value
 
+	#CLUSTERING IS COMPLETEED HERE DO THE PREDICTION
+	#Normalize database.csv and use norm.p that i sent
+
+	# Load The song DataBase
+	database = load("database.csv")
+
+	#Normalize
+
 main()
+
